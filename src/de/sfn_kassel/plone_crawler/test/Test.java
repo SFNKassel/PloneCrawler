0f47a8ab03f768d7f9b9364313268631329c7eb1
Anselm von Wangenheim <anselm.w@gmx.de>
2015-07-07 19:01:56 +0200

    initial commit

---
+++ src/de/sfn_kassel/plone_crawler/test/Test.java
@@ -0,0 +1,10 @@
+package de.sfn_kassel.plone_crawler.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(42);
+	}
+
+}
